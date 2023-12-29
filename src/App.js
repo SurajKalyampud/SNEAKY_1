d9a1005b9a392d356a02e0a90870088283f79a6e
SurajKalyampud <146307048+SurajKalyampud@users.noreply.github.com>
2023-12-29 13:23:03 -0800

    new commit3

--- src/App.js
+++ src/App.js
@@ -18,12 +18,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/surajkalyampud" element={<Home />} />
         <Route exact path="/SNEAKY_MAIN" element={<Home />} />
-        <Route
-          exact
-          path="/surajkalyampud.github.io/SNEAKY_MAIN/"
-          element={<Home />}
-        />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/login" element={<Login />} />
